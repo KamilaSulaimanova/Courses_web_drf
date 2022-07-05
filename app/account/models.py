7fb8c0d9bb8e473ebda8ae764e50adb8bff8cb33
Kamila Sulaimanova <kamilasulaimanova@Kamilas-MacBook-Pro.local>
2022-07-05 19:53:01 +0600

    final commit

--- app/account/models.py
+++ app/account/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     username = models.EmailField(max_length=128, verbose_name='email', unique=True)
-    profile_image = models.ImageField(upload_to='user/', default='user/default.png')
+    # profile_image = models.ImageField(upload_to='user/', default='user/default.png')
 
 
     def save(self, *args, **kwargs):
--- app/course/settings.py
+++ app/course/settings.py
@@ -10,29 +10,25 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from pathlib import Path
+
 import os
-import environ
+from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-env = environ.Env(DEBUG=(bool, False))
-
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = os.environ.get('DEBUG', default=0)
 
-ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
-
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
 # Application definition
 
@@ -84,18 +80,24 @@ WSGI_APPLICATION = 'course.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-
-   'default': {
-       'ENGINE': 'django.db.backends.mysql',
-       'NAME': env("DB_NAME"),
-       'USER': env("DB_USER"),
-       'PASSWORD': env("DB_PASSWORD"),
-       'HOST': 'localhost',
-       'CHARSET': 'utf8',
-       'COLLATION': 'utf8_general_ci',
-   }
-
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE"),
+        "NAME": os.environ.get("SQL_DATABASE"),
+        "USER": os.environ.get("SQL_USER"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD"),
+        "HOST": os.environ.get("SQL_HOST"),
+        "PORT": os.environ.get("SQL_PORT"),
+    }
 }
+
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
